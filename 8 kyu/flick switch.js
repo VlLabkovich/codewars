a79da76a8a9e2d27182959a273f612b7093a107c
Vladislav <labkovich.vl@gmail.com>
2023-10-12 12:02:50 +0300

    init: start task flick switch

---
+++ 8 kyu/flick switch.js
@@ -0,0 +1,21 @@
+// DESCRIPTION:
+// Create a function that always returns true for every item in a given list. 
+// However, if an element is the word "flick", switch to always returning the opposite boolean value.
+
+// Notes:
+// - "flick" will always be given in lowercase.
+// - A list may contain multiple flicks.
+// - Switch the boolean value on the same element as the flick itself.
+
+// SOLUTION:
+
+function flickSwitch(arr) {
+    let boolean = true;
+    return arr.map(el => {
+        if (el === 'flick') {
+            boolean = !boolean;
+        };
+        return boolean;
+    });
+};
+console.log(flickSwitch(["codewars", "flick", "code", "wars", "flick"]));
