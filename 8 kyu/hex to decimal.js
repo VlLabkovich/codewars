0a468c23fd57a9755d86057277acf3026189dadb
Vladislav <labkovich.vl@gmail.com>
2023-10-19 15:19:48 +0300

    feat: add hex to decimal

--- 8 kyu/hex to decimal.js
+++ 8 kyu/hex to decimal.js
@@ -2,3 +2,9 @@
 // Complete the function which converts hex number (given as a string) to a decimal number.
 
 // SOLUTION:
+
+function hexToDecimal(hex) {
+    return parseInt(hex, 16);
+}
+
+console.log(hexToDecimal("a"));
