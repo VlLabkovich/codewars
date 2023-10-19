0f77e8a112369cb3bffd67fc7f036f2d7c852722
Vladislav <labkovich.vl@gmail.com>
2023-10-19 10:40:14 +0300

    init: start task merge sort integer arrays

---
+++ 8 kyu/merging sorted integer arrays (without duplicates).js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// Write a function that merges two sorted arrays into a single one. The arrays only contain integers.
+// Also, the final outcome must be sorted and not have any duplicate.
+
+// SOLUTION:
