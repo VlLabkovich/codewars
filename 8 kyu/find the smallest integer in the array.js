f5a2aad6a66419639a6abed1f081f467f8e7f4dc
Vladislav <labkovich.vl@gmail.com>
2023-10-19 10:11:24 +0300

    feat: add find smallest integer

--- 8 kyu/find the smallest integer in the array.js
+++ 8 kyu/find the smallest integer in the array.js
@@ -2,3 +2,12 @@
 // Given an array of integers your solution should find the smallest integer.
 
 // SOLUTION:
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+         return Math.min(...args);
+     };
+ };
+ const object = new SmallestIntegerFinder();
+ 
+ console.log(object.findSmallestInt([78, 0, 232, -12, 8, -14]));
