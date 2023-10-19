c4d0337ad71e33301940635b41b1335efd0f7ac2
Vladislav <labkovich.vl@gmail.com>
2023-10-19 16:44:24 +0300

    feat: add bin to decimal

--- 8 kyu/bin to decimal.js
+++ 8 kyu/bin to decimal.js
@@ -2,3 +2,7 @@
 // Complete the function which converts a binary number (given as a string) to a decimal number.
 
 // SOLUTION:
+
+let binToDec = (bin) => {return parseInt(bin, 2)}
+
+console.log(binToDec("010101"));
