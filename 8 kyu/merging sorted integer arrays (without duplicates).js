93c990780766ca3ece965200213e1c1b1776f6bb
Vladislav <labkovich.vl@gmail.com>
2023-10-19 10:42:50 +0300

    feat: add merge sort integer

--- 8 kyu/merging sorted integer arrays (without duplicates).js
+++ 8 kyu/merging sorted integer arrays (without duplicates).js
@@ -3,3 +3,9 @@
 // Also, the final outcome must be sorted and not have any duplicate.
 
 // SOLUTION:
+
+function mergeArrays(a, b) {
+    return [...new Set(([].concat(a,b)).sort((a, b) => a - b))];
+}
+
+console.log(mergeArrays([1,5,1,4,2], [5,8,1,9,4]));
