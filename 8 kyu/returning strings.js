57ff9956aafd4ef28922f36e02bc0a9c3890375f
Vladislav <labkovich.vl@gmail.com>
2023-10-19 15:08:03 +0300

    feat: add return string

--- 8 kyu/returning strings.js
+++ 8 kyu/returning strings.js
@@ -2,3 +2,9 @@
 // Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
 // SOLUTION:
+
+function greet(name){
+    return `"Hello, ${name} how are you doing today?"`;
+}    
+
+console.log(greet("Vlad"));
