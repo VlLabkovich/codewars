0b40a8aaaf9e7d5c14f5042cd58c88e603309643
Vladislav <labkovich.vl@gmail.com>
2023-10-19 10:16:43 +0300

    feat: add flick switch

--- 8 kyu/flick switch.js
+++ 8 kyu/flick switch.js
@@ -8,3 +8,14 @@
 // - Switch the boolean value on the same element as the flick itself.
 
 // SOLUTION:
+
+function flickSwitch(arr) {
+    let boolean = true;
+    return arr.map(el => {
+        if (el === 'flick') {
+            boolean = !boolean;
+        };
+        return boolean;
+    });
+}
+console.log(flickSwitch(["codewars", "flick", "code", "wars", "flick"]));
