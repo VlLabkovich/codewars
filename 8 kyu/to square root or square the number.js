72bdd2fa08297d6ba6d1b87e66f4adddbe61c437
Vladislav <labkovich.vl@gmail.com>
2023-10-25 16:03:10 +0300

    feat: add to square root or square the number

--- 8 kyu/to square root or square the number.js
+++ 8 kyu/to square root or square the number.js
@@ -8,3 +8,9 @@
 // The input array will always contain only positive numbers, and will never be empty or null.
 
 // SOLUTION:
+
+function squareOrSquareRoot(array) {
+    return  array.map(el => Number.isInteger(Math.sqrt(el)) ? Math.sqrt(el) : el * el);
+    }
+
+    console.log(squareOrSquareRoot([ 4, 3, 9, 7, 2, 1 ]));
