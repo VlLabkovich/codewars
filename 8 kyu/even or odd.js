7d5283d2ebde53e1061c352dde31eb58c9f9fbfe
Vladislav <labkovich.vl@gmail.com>
2023-10-26 19:54:23 +0300

    feat: add even or odd return numbers

--- 8 kyu/even or odd.js
+++ 8 kyu/even or odd.js
@@ -2,3 +2,7 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 // SOLUTION:
+
+const evenOrOdd = (number) => number % 2 === 0? "Even" : "Odd";
+
+console.log(evenOrOdd(10));
