e319067e34974470a8dd2e6dd805206721a20779
Vladislav <labkovich.vl@gmail.com>
2023-10-28 19:46:56 +0300

    feat: add check two give characters

--- 8 kyu/check same case.js
+++ 8 kyu/check same case.js
@@ -11,3 +11,17 @@
 // '0' and '?' returns -1
 
 // SOLUTION:
+
+function sameCase(a, b) {
+
+    if (!(/^[a-zA-Z]$/.test(a)) || !(/^[a-zA-Z]$/.test(b))) {
+        return -1;
+    } else if ((/^[A-Z]$/.test(a) && /^[A-Z]$/.test(b)) || (/^[a-z]$/.test(a) && /^[a-z]$/.test(b))) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+}
+
+console.log(sameCase('A', 'B'));
