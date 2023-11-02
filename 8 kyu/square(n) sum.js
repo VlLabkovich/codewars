2f72e4539abf25ef880c0b9a1dfc176eb400b3c7
Vladislav <labkovich.vl@gmail.com>
2023-11-02 22:31:40 +0300

    feat: add square sum function

--- 8 kyu/square(n) sum.js
+++ 8 kyu/square(n) sum.js
@@ -2,3 +2,7 @@
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 // SOLUTION:
+
+const squareSum = numbers => (numbers.map(el => Math.pow(el, 2))).reduce((acc, cur) => acc + cur, 0);
+
+console.log(squareSum([1,2,2]));
