a2cfd7cdd3d614c2d6d13d5213d3024dd81c84f4
Vladislav <labkovich.vl@gmail.com>
2023-11-04 00:03:44 +0300

    feat: add function count sheep

--- 8 kyu/counting sheep.js
+++ 8 kyu/counting sheep.js
@@ -2,3 +2,11 @@
 // Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
 // SOLUTION:
+
+const countSheeps = (sheep) => {
+    let count = 0;
+    sheep.forEach(el => el === true? count+=1 : false);
+    return count;
+}
+
+console.log(countSheeps([true,true,true,false,true,true,true,true,true,false,true,false,true,false,false,true,true,true,true,true,false,false,true,true]));
