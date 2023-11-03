bf7c0a249b706587332956136f445421360e1663
Vladislav <labkovich.vl@gmail.com>
2023-11-03 23:08:25 +0300

    feat: add function divide for two number

--- 8 kyu/is n divisible by x and y.js
+++ 8 kyu/is n divisible by x and y.js
@@ -2,3 +2,7 @@
 // Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
 
 // SOLUTION:
+
+const isDivisible = (n, x, y) => (Number.isInteger(n / x) && Number.isInteger(n / y))? true : false;
+
+console.log(isDivisible(8,3,4));
