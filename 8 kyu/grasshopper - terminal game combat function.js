583ff9888dc8c5fa9f031354d28ec2ceed573af9
Vladislav <labkovich.vl@gmail.com>
2023-11-07 22:39:18 +0300

    feat: add return function new health

--- 8 kyu/grasshopper - terminal game combat function.js
+++ 8 kyu/grasshopper - terminal game combat function.js
@@ -2,3 +2,7 @@
 // Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
 
 // SOLUTION:
+
+const combat = (health, damage) =>(health - damage) <= 0 ? 0 : health - damage;
+
+console.log(combat(100, 95));
