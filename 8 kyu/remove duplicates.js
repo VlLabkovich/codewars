d5c05287170afa6ea5ff9b540c5f5ca0bdc1ec31
Vladislav <labkovich.vl@gmail.com>
2023-11-07 22:02:40 +0300

    init: start task remove duplicates from array

---
+++ 8 kyu/remove duplicates.js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+// The order of the sequence has to stay the same.
+
+// SOLUTION:
