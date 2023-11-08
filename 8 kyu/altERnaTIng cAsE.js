3f39cffbed948744b3ac47e234136a239b71910b
Vladislav <labkovich.vl@gmail.com>
2023-11-08 23:32:04 +0300

    feat: add change register string

--- 8 kyu/altERnaTIng cAsE.js
+++ 8 kyu/altERnaTIng cAsE.js
@@ -3,3 +3,11 @@
 // As usual, your function/method should be pure, i.e. it should not mutate the original string.
 
 // SOLUTION:
+
+String.prototype.toAlternatingCase = function () {
+    let objStr = Object.values(this);
+    let newArr = objStr.map((el) => el === el.toUpperCase() ? el.toLowerCase() : el.toUpperCase());
+    return newArr.join('');
+}
+
+console.log("hElLo WoRlD".toAlternatingCase());
