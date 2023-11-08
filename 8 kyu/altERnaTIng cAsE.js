a41148852ae719dc4467941455927a173c7c9b91
Vladislav <labkovich.vl@gmail.com>
2023-11-08 23:29:49 +0300

    init: start task change register string

---
+++ 8 kyu/altERnaTIng cAsE.js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
+// SOLUTION:
