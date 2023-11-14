8725e357a9217972c579b985acfd7a8f25915679
Vladislav <labkovich.vl@gmail.com>
2023-11-14 23:24:45 +0300

    feat: create function for find element

--- 8 kyu/a needle in the haystack.js
+++ 8 kyu/a needle in the haystack.js
@@ -7,3 +7,15 @@
 // ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
 
 // SOLUTION:
+
+function findNeedle(haystack) {
+    let newArr;
+    haystack.forEach((el, index) => {
+      if (el === 'needle') {
+        newArr = 'found the '+ el + ' at position ' + index;
+      }
+    })
+    return newArr;
+  }
+
+  console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]));
