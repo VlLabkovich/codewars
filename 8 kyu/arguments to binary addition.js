618fb1199ae7df315e0a44ac4c593c023349cdfe
Vladislav <labkovich.vl@gmail.com>
2023-11-16 23:34:27 +0300

    feat: add return binary equivalent the sum

--- 8 kyu/arguments to binary addition.js
+++ 8 kyu/arguments to binary addition.js
@@ -7,3 +7,7 @@
 // NOTE: NaN is a number too in javascript for decimal, binary and n-ary base
 
 // SOLUTION:
+
+const arr2bin = arr => arr.filter(el => typeof el == 'number').reduce((acc, cur) => acc + cur, 0).toString(2);
+
+console.log(arr2bin([1,10,100,1000,"A",[]]));
