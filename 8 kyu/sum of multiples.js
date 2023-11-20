a9d50aeada80b6dfec0bb214c0efb1d3240926a8
Vladislav <labkovich.vl@gmail.com>
2023-11-20 23:43:25 +0300

    feat: add sum all multiples n

--- 8 kyu/sum of multiples.js
+++ 8 kyu/sum of multiples.js
@@ -4,3 +4,16 @@
 // m is excluded from the multiples.
 
 // SOLUTION:
+
+function sumMul(n, m) {
+    let sum = 0;
+    for (let i = n; i < m; i++) {
+        if (i % n === 0) {
+            sum += i;
+        }
+    }
+   return (n && m) <= 0 ? "INVALID" : sum;
+}
+
+console.log(sumMul(2,9));
+console.log(sumMul(2,-9));
