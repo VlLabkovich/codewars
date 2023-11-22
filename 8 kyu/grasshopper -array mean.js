b2907423d3343f4e3b1431ca03de87eb9682a0a2
Vladislav <labkovich.vl@gmail.com>
2023-11-22 22:28:44 +0300

    feat: add find the mean

--- 8 kyu/grasshopper -array mean.js
+++ 8 kyu/grasshopper -array mean.js
@@ -2,3 +2,7 @@
 // Find the mean (average) of a list of numbers in an array.
 
 // SOLUTION:
+
+const findAverage = nums => nums.reduce((acc, cur) => (acc + cur), 0) / nums.length;
+
+console.log(findAverage([3, 7, 2, 4]));
